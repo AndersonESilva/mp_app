e8331741ca644d2be0e7a84ac87bc230be6bff79
andersonsilvazup <anderson.silva@zup.com.br>
2019-07-15 13:57:40 -0300

    Class de strings Br, e profilePage iniciada

--- lib/feature/profile/profilePage.dart
+++ lib/feature/profile/profilePage.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:mp_app/useful/languageBr.dart';
 
-class ProfilePage extends StatelessWidget{
+class ProfilePage extends StatelessWidget {
+  List _toDoList = ["aaa"];
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
-          child: Center(
-            child: Image.asset('images/ferrari.jpg'),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.only(top: 40),
+          child: CircleAvatar(
+            radius: 70,
+              child: ClipOval(
+                child: SizedBox(
+                  width: 140,
+                  height: 140,
+                  child: Image.network("https://abrilexame.files.wordpress.com/2018/10/capaprofile.jpg",
+                  fit: BoxFit.fill,),
+                ),
+              )),
+        ),
+        Container(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            children: <Widget>[
+              Text(LanguageBr.profilePage_text_name,
+                  style: TextStyle(
+                    fontSize: 25.0
+                  )),
+              Text(LanguageBr.profilePage_text_Local,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  )),
+            ],
           ),
-        )
+        ),
       ],
     );
   }
-
 }
---
+++ lib/useful/languageBr.dart
@@ -0,0 +1,6 @@
+
+class LanguageBr{
+
+  static final profilePage_text_name = "Nome Sobrenome";
+  static final profilePage_text_Local = "Local Atual";
+}
