53649971b7cdcddd61ca81b479523ea784cd7b97
andersonsilvazup <anderson.silva@zup.com.br>
2019-07-16 16:43:00 -0300

    Ajuste na organização do código

--- lib/feature/profile/profilePage.dart
+++ lib/feature/profile/profilePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mp_app/useful/languageBr.dart';
 
 class ProfilePage extends StatelessWidget {
+
   List _toDoList = ["aaa", "sdsds", "fsadfa", "dwa", "dwa", "dwa", "dwa"];
 
   @override
@@ -94,56 +95,56 @@ class ProfilePage extends StatelessWidget {
           })),
     ));
   }
-}
 
-Widget _buildItems(int index) {
-  return Card(
-    child: Column(
-      children: <Widget>[
-        Container(
-            alignment: Alignment.topCenter,
-            width: double.maxFinite,
-            height: 120.0,
-            child: Image.asset(
-              'images/ferrari.jpg',
-              fit: BoxFit.fill,
-            )),
-        Container(
-            margin: const EdgeInsets.only(left: 10),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.only(top: 5,bottom: 5),
-                    child: Text(
-                      LanguageBr.profilePage_text_event_name,
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 5, bottom: 5),
-                    child: Text(
-                      LanguageBr.profilePage_text_event_local,
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                      ),
-                    ),
-                  ),
-                  Container(
-                      margin: const EdgeInsets.only(bottom: 5),
+  Widget _buildItems(int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.topCenter,
+              width: double.maxFinite,
+              height: 120.0,
+              child: Image.asset(
+                'images/ferrari.jpg',
+                fit: BoxFit.fill,
+              )),
+          Container(
+              margin: const EdgeInsets.only(left: 10),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(top: 5,bottom: 5),
                       child: Text(
-                        LanguageBr.profilePage_text_event_date,
+                        LanguageBr.profilePage_text_event_name,
                         style: TextStyle(
                           color: Colors.grey[500],
                         ),
-                      )),
-                ],
-              ),
-            ))
-      ],
-    ),
-  );
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Text(
+                        LanguageBr.profilePage_text_event_local,
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(bottom: 5),
+                        child: Text(
+                          LanguageBr.profilePage_text_event_date,
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                          ),
+                        )),
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+  }
 }
