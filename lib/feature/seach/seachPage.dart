097e2f947268c3ae906c2b30f163fca05fe5a8f7
andersonsilvazup <anderson.silva@zup.com.br>
2019-07-17 12:13:22 -0300

    Implementado tabview

--- lib/feature/seach/seachPage.dart
+++ lib/feature/seach/seachPage.dart
@@ -1,29 +1,42 @@
-
 import 'package:flutter/material.dart';
 
-class SeachPage extends StatelessWidget{
-
+class SeachPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: <Widget>[
-        TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso (Kg)",
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 20.00),
-            //controller: weightControl,
-            validator: (value) {
-              if (value.isEmpty) {
-                return "Insira seu Peso:";
-              }
-            }),
+        Expanded(
+          child: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.white12,
+                elevation: 0.0,
+                flexibleSpace: SafeArea(
+                  child: _getTabBar(),
+                ),
+              ),
+              body: TabBarView(
+                children: [
+                  Icon(Icons.directions_car),
+                  Icon(Icons.directions_transit)
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
 
-
+  Widget _getTabBar(){
+    return TabBar(
+      labelColor: Colors.grey[500],
+      indicatorColor: Colors.grey[500],
+      tabs: [
+        Tab(text: "promo"),
+        Tab(text: "evnt")
+      ],
+    );
+  }
 }
