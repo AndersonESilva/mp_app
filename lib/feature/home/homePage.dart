d9285cc9a2504fa5c03df6fedaaef824091a414e
AndersonESilva <andersoneraldodasilva@live.com>
2019-10-24 13:59:22 -0300

    HomePage separada

---
+++ lib/feature/home/homePage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:mp_app/feature/feed/feedPage.dart';
+import 'package:mp_app/feature/feed/feedAppBar.dart';
+import 'package:mp_app/feature/profile/profileAppBar.dart';
+import 'package:mp_app/feature/profile/profilePage.dart';
+
+class HomePage extends State{
+
+  BuildContext _context;
+
+  final List<Widget> _children = [
+    FeedPage(),
+    ProfilePage()
+  ];
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    _context = context;
+    return Scaffold(
+        appBar: _getAppBar(),
+        body: RefreshIndicator(
+            onRefresh: _refresh,
+            child: _children[_currentIndex]
+        ),
+        bottomNavigationBar: _buildBottomNavigationBar());
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text(''),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.dehaze),
+          title: Text(''),
+        ),
+      ],
+      currentIndex: _currentIndex,
+      fixedColor: Colors.black,
+      onTap: (index){
+        _onTabTapped(index);
+      },
+    );
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    return null;
+  }
+
+  AppBar _getAppBar() {
+    switch (_currentIndex) {
+      case 0:
+        {
+          return FeedAppBar().build(_context);
+        }
+      case 1:
+        {
+          return ProfileAppBar().build(_context);
+        }
+    }
+  }
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+  
+}
