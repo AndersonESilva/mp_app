9c703a0d494b17a9f28e68d0a513dd025247383f
AndersonESilva <andersoneraldodasilva@live.com>
2019-10-24 15:20:39 -0300

    Arquivo de  login criado

---
+++ lib/feature/login/loginPage.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: MaterialButton(
+        color: Colors.blue,
+        child: Text(
+          'LOGIN'
+        ),
+      ),
+    );
+  }
+
+}
