f6be86d9a28485e162747631f5cda7130a14f8b6
TMA00422 <anderson.silva@terceiro-sky.com.br>
2019-10-25 16:56:49 -0300

    Tela do evento implementação básica

--- lib/feature/event/eventPage.dart
+++ lib/feature/event/eventPage.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class EventPage extends StatelessWidget{
+class EventPage extends StatelessWidget {
+  List _toDoList = ["aaa", "sdsds", "fsadfa", "dwa", "dwa", "dwa", "dwa"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+        appBar: AppBar(
+          title: Text("Second Route"),
         ),
-      ),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverList(
+              delegate: SliverChildListDelegate([_buildEvent()]),
+            ),
+            SliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  return _buildImages();
+                }, childCount: _toDoList.length))
+          ],
+        ));
+  }
+
+  Widget _buildEvent() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              width: double.maxFinite,
+              height: 260.0,
+              padding: const EdgeInsets.only(top: 10),
+              child: Image.network(
+                "https://www.privilegebrasil.com//conteudo/anexo/BANNER4.jpg",
+                fit: BoxFit.fill,
+              )),
+          _buildInfo(),
+        ]);
+  }
+
+  Widget _buildInfo() {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Nome do evento', textAlign: TextAlign.right),
+        Text('Endereço'),
+        Text('Data'),
+      ],
     );
   }
 
-
+  Widget _buildImages() {
+    return Card(
+        child: Container(
+            width: double.maxFinite,
+            height: 120.0,
+            child: Image.network(
+              'http://www.voicers.com.br/wp-content/uploads/2018/09/menos-30-fest-fortaleza-banner-1.jpg',
+              fit: BoxFit.fill,
+            )));
+  }
 }
