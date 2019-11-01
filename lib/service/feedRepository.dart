922072e0eb89953478e66e35a71f76f6c46799f4
TMA00422 <anderson.silva@terceiro-sky.com.br>
2019-11-01 17:11:59 -0300

    Implementação básica de requisição

--- lib/service/feedRepository.dart
+++ lib/service/feedRepository.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:mp_app/data/model/eventResponse.dart';
 
 class FeedRepository{
 
-  Future<http.Response> fetchEvents() async {
-    return http.get('https://jsonplaceholder.typicode.com/posts/1');
+  Future<EventResponse> fetchEvents() async {
+    final response = await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+    if(response.statusCode == 200){
+      return EventResponse.fromJson(json.decode(response.body));
+    }else{
+      throw Exception('Failed to load post');
+    }
   }
 }
