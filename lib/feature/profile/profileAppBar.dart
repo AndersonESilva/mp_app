74544046537bebfba0babf696c48c4b5b28890bf
AndersonESilva <andersoneraldodasilva@live.com>
2020-01-23 09:47:11 -0300

    Mudança de icone em tela de perfil

--- lib/feature/profile/profileAppBar.dart
+++ lib/feature/profile/profileAppBar.dart
@@ -10,7 +10,7 @@ class ProfileAppBar extends StatelessWidget{
       actions: <Widget>[
         IconButton(
           color: Colors.black,
-          icon: Icon(Icons.settings),
+          icon: Icon(Icons.close),
           onPressed: (){},
         )
       ],
